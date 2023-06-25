3eada3e193aa6d4461ba4b660ae6ebf335852c06
vkitori <vikibibetko@gmail.com>
2023-06-25 19:47:30 +0200

    project controller added

--- demo/src/main/java/com/kanbantool/demo/domain/Project.java
+++ demo/src/main/java/com/kanbantool/demo/domain/Project.java
@@ -25,102 +25,70 @@ public class Project {
 		
 	}
 	
-	
-	
 	public Long getId() {
 		return id;
 	}
-
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
-
 	public String getProjectName() {
 		return projectName;
 	}
-
-
 
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
 	}
 
-
-
 	public String getProjectIdentifer() {
 		return projectIdentifer;
 	}
-
-
 
 	public void setProjectIdentifer(String projectIdentifer) {
 		this.projectIdentifer = projectIdentifer;
 	}
 
-
-
 	public String getDescription() {
 		return description;
 	}
-
-
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
-
 	public Date getStart_date() {
 		return start_date;
 	}
-
-
 
 	public void setStart_date(Date start_date) {
 		this.start_date = start_date;
 	}
 
-
-
 	public Date getEnd_date() {
 		return end_date;
 	}
-
-
 
 	public void setEnd_date(Date end_date) {
 		this.end_date = end_date;
 	}
 
-
-
 	public Date getCreated_At() {
 		return created_At;
 	}
 
-
-
 	public void setCreated_At(Date created_At) {
 		this.created_At = created_At;
 	}
-
-
 
 	public Date getUpdated_At() {
 		return updated_At;
 	}
 
 
-
 	public void setUpdated_At(Date updated_At) {
 		this.updated_At = updated_At;
 	}
-
 
 
 	@PrePersist
---
+++ demo/src/main/java/com/kanbantool/demo/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.kanbantool.demo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kanbantool.demo.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository <Project, Long>{
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> iterable);
+	 
+}
