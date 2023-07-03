e34ab710b260854e2e47432a04ac10e7793099f1
vkitori <vikibibetko@gmail.com>
2023-07-03 15:00:12 +0200

    map validation error service added

--- demo/src/main/java/com/kanbantool/demo/web/ProjectController.java
+++ demo/src/main/java/com/kanbantool/demo/web/ProjectController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kanbantool.demo.domain.Project;
+import com.kanbantool.demo.services.MapValidationErrorService;
 import com.kanbantool.demo.services.ProjectService;
 
 import jakarta.validation.Valid;
@@ -26,18 +27,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, 
 			BindingResult result){
-			if (result.hasErrors()) {
-				
-				Map<String, String> errorMap = new HashMap<>();
-				for(FieldError error: result.getFieldErrors()) {
-					errorMap.put(error.getField(), error.getDefaultMessage());
-				}
-				
-				return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+			
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result); 
+		if(errorMap != null) return errorMap;
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
