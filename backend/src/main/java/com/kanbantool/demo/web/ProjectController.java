2fa945eba6ddc6faf03cb4097d1ebd3413ea69cf
Viktoria <vikibibetko@gmail.com>
2023-10-19 21:03:58 +0200

    added creating project function from react

--- backend/src/main/java/com/kanbantool/demo/web/ProjectController.java
+++ backend/src/main/java/com/kanbantool/demo/web/ProjectController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
 	
 	@Autowired
