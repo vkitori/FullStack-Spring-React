2186637edb3a900a2e01bb061bf3d2863b708415
Viktoria <vikibibetko@gmail.com>
2023-11-09 19:10:54 +0100

    added delete function

--- ppmtool-client/src/actions/projectActions.js
+++ ppmtool-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECT, GET_PROJECTS } from "./types";
+import { GET_ERRORS, GET_PROJECT, GET_PROJECTS, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
     try {
@@ -41,4 +41,12 @@ export const getProject = (id, history) => async dispatch => {
     } catch (error) {
         history.push("/dashboard");
     }
+};
+
+export const deleteProject = id => async dispatch => {
+    await axios.delete(`http://localhost:8080/api/project/${id}`);
+    dispatch({
+        type: DELETE_PROJECT,
+        payload: id
+    });
 };
